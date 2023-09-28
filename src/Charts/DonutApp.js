a988a11265aff0422ce333c5fb77e81997d51cd5
EMrahuladhikari <rahul.adhikari@eminds.ai>
2023-09-28 16:44:00 +0530

    first commit

--- src/Charts/DonutApp.js
+++ src/Charts/DonutApp.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 function DonutApp() {
     const [data,setData] = useState([]);
     useEffect(() => {
-        const apiurl ='http://192.168.141.147:8000/api/saw-machines';
+        const apiurl ='https://127.0.0.1:8000/api/saw-machines';
 
         axios.get(apiurl)
          .then((response)=>{
