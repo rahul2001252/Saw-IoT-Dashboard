75ab5e10b48d7a3976ade83c76ddf5a1fabacf8f
rahul2001252 <rahul.n.adhikari2001@gmail.com>
2023-10-06 10:09:40 +0530

    v1 deploy build

--- src/config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-
-export const BASE_URL = 'http://192.168.182.147:8000';
-export const CERT_PATH = 'C:/Users/hp/Desktop/CGPT/iot-dashboard/cert.crt'; // Specify the correct path to your certificate file
-export const KEY_PATH = 'C:/Users/hp/Desktop/CGPT/iot-dashboard/cert.key';   // Specify the correct path to your key file
